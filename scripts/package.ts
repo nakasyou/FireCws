096f72180e04aec93b0b9f4a29940cd0e46fdd48
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-06 20:18:14 +0900

    バージョン指定

--- scripts/package.ts
+++ scripts/package.ts
@@ -1,6 +1,12 @@
 import * as fflate from "https://esm.sh/fflate@0.8.0"
 import * as fs from "https://deno.land/std@0.196.0/fs/mod.ts"
 
+const version = Deno.args[0]
+
+const manifest = JSON.parse(await Deno.readTextFile('./extension/manifest.json'))
+manifest.version = version
+await Deno.writeTextFile('./extension/manifest.json', JSON.stringify(manifest, null, 2))
+
 const files: Record<string, Uint8Array> = {}
 for await (const entry of fs.walk("./extension")) {
   if (!entry.isFile) {
